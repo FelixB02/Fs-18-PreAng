de7090b12b950dda0adf5a29596928424e5a8862
FelixB02 <felix.blechl@chello.at>
2023-02-20 10:39:06 +0100

    All files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent} from './home-page/home-page.component';
 
-const routes: Routes = [];
+import { AboutPageComponent} from  './about-page/about-page.component';
+
+const routes: Routes = [
+
+  {
+
+          path:"",component: HomePageComponent
+
+  },
+
+  {
+
+          path: "About",component: AboutPageComponent
+
+  }
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+
+  selector: 'navbar',
+
+  templateUrl: './navbar.component.html',
+
+  styleUrls: ['./navbar.component.css']
+
+})
+
+export class NavbarComponent implements OnInit {
+  projectName:string = "CodeFactory";
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
